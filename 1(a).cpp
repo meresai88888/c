5e80e71d5f3370386599ff5438833cc0d53ffbcb
meresai88888 <121922231+meresai88888@users.noreply.github.com>
2023-01-04 11:18:17 +0530

    Add files via upload

---
+++ 1(a).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"entera,b";
+	cin>>a>>b;
+	cout<<"addition="<<a+b<<endl;
+	cout<<"subtraction="<<a-b<<endl;
+	cout<<"multiplication="<<a*b<<endl;
+	cout<<"division="<<a/b<<endl;
+	if(a>b)
+    	cout<<"a is bigger";
+	else
+		cout<<"b is bigger";
+	return 0;
+		
+}
